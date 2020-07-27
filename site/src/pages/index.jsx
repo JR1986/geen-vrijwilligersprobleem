e3732b6c73412138101eee53e6f07edb6ff9f3de
Jelle <jelle@fashionunited.com>
2020-07-27 09:16:23 +0200

    Fix path SEO file

--- site/src/pages/index.jsx
+++ site/src/pages/index.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { StaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import SEO from "../components/Seo"
 
 const IndexPage = () => (
   <Layout>
